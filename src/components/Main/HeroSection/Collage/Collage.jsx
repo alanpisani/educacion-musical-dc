7a37a470f60a564946208a78381c44f5672ee280
Alan Pisani <alanpisani1993@gmail.com>
2025-08-07 17:59:23 -0300

    componente Collage semicompletado

--- src/components/Main/HeroSection/Collage/Collage.jsx
+++ src/components/Main/HeroSection/Collage/Collage.jsx
@@ -1,9 +1,16 @@
 import "./Collage.css"
+import fotoUno from "../../../../assets/img/foto-1.png";
+import fotoDos from "../../../../assets/img/foto-2.png";
+import fotoTres from "../../../../assets/img/foto-3.png";
+import fotoCuatro from "../../../../assets/img/foto-4.jpg";
 
 function Collage(){
     return (
         <div className="collage">
-            <img src="../../asets/img/foto-1.png" alt="" />
+            <img src={fotoUno} alt="" />
+            <img src={fotoDos} alt="" />
+            <img src={fotoTres} alt="" />
+            <img src={fotoCuatro} alt="" />
         </div>
     );
 }
