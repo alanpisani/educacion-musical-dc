ba264209ff2eef0e267ec48cf7e45488903d29e9
Alan Pisani <alanpisani1993@gmail.com>
2025-08-07 17:25:33 -0300

    Componente 'Collage' basico creado

---
+++ src/components/Main/HeroSection/Collage/Collage.jsx
@@ -0,0 +1,11 @@
+import "./Collage.css"
+
+function Collage(){
+    return (
+        <div className="collage">
+            <img src="../../asets/img/foto-1.png" alt="" />
+        </div>
+    );
+}
+
+export default Collage;
--- src/components/Main/HeroSection/HeroSection.jsx
+++ src/components/Main/HeroSection/HeroSection.jsx
@@ -1,24 +1,24 @@
 import "./HeroSection.css";
+import Collage from "./Collage/Collage";
+
 
 import InfoContainer from "../../shared/InfoContainer/InfoContainer";
 
 function HeroSection(){
     return(
         <section className="hero-section">
-            <div className="hero-info-container">
-                <InfoContainer subtitle="Profe Daiana Chaparro"
-                    titleChildren={
-                        <h1>Educación Musical DC</h1>
-                    }
-                    description={"lorem ipsu kljask jkjkljka kjaksjd kjk laksjd  jklasd jklasssl  lkjklas   lkjaklsd k jklasd lk jaks  lkjaskldk k jkals"}
-                    labelBtn={"Contacto"}
-                />
+            <div className="section-container">
+                <div className="hero-info-container">
+                    <InfoContainer subtitle="Profe Daiana Chaparro"
+                        titleChildren={
+                            <h1>Educación Musical DC</h1>
+                        }
+                        description={"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis officiis deserunt quasi dolorum repudiandae quaerat enim dicta quam id pariatur ratione neque ea accusantium consectetur optio accusamus, quos incidunt perspiciatis?"}
+                        labelBtn={"Contacto"}
+                    />
+                </div>
+                <Collage></Collage>
             </div>
-            <div className="collage">
-                
-            </div>
-
-            
         </section>
     );
 }
