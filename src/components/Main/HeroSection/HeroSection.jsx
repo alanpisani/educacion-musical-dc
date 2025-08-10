ccab4e6e9350b8198e1fcda30f5bfc60fc920211
Alan Pisani <alanpisani1993@gmail.com>
2025-08-10 15:59:18 -0300

    Rediseniando hero-section

--- src/components/Main/HeroSection/HeroSection.jsx
+++ src/components/Main/HeroSection/HeroSection.jsx
@@ -2,19 +2,19 @@ import "./HeroSection.css";
 import Collage from "./Collage/Collage";
 
 
-import InfoContainer from "../../shared/InfoContainer/InfoContainer";
+import CoshitaButton from "../../shared/CoshitaButton/CoshitaButton"
 
 function HeroSection(){
     return(
         <section className="hero-section">
             <div className="section-container">
                 <div className="hero-info-container">
-                    <InfoContainer subtitle="Profe Daiana Chaparro"
-                        titleChildren={
-                            <h1>Educación Musical DC</h1>
-                        }
-                        description={"Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis officiis deserunt quasi dolorum repudiandae quaerat enim dicta quam id pariatur ratione neque ea accusantium consectetur optio accusamus, quos incidunt perspiciatis?"}
-                        labelBtn={"Contacto"}
+                    <h1>Clases de Música</h1>
+                    <h2 className="educacion-musical">Edudación Musical DC</h2>
+                    <h3 className="profe">Profe Daiana Chaparro</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut ut delectus blanditiis aspernatur dolorum aliquid nostrum quod placeat cumque mollitia? Maiores neque similique cum labore expedita aliquam blanditiis inventore facilis?</p>
+                    <CoshitaButton label={"Contacto"}
+                    
                     />
                 </div>
                 <Collage></Collage>
